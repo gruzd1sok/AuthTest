332486e00e92edd56a361052bf7d0e45d0a2b51b
Ilya Gruzdev <ily.gruzdev99@gmail.com>
2021-06-10 18:54:08 +0700

    Initial Commit

---
+++ AuthTest/AuthTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthTestApp.swift
+//  AuthTest
+//
+//  Created by Илья Груздев on 10.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct AuthTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
